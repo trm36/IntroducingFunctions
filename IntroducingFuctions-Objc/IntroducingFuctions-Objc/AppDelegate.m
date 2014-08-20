1c661881566dc3fd8eacd441e73ce6cf0120b488
Taylor Mott <trm36@humboldt.edu>
2014-08-20 10:10:41 -0600

    Functions Practice Pre DevMt
    Taylor Mott
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -17,13 +17,42 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
+    NSString *myName = @"Taylor Mott";
+    NSString *introduction = [NSString stringWithFormat:@"My name is %@", myName];
+    NSString *japaneseIntroduction = [myName stringByAppendingString:@" to moushimasu"];
     
+    NSLog(@"%@", introduction);
+    NSLog(@"%@", japaneseIntroduction);
+    
+    [self countDownTillIntroduction: 4];
+    
+    [self printIntroductions: myName];
     
     return YES;
 }
 
-// Define new functions here...
+- (void)printIntroductions:(NSString*)name
+{
+    NSString *introduction = [NSString stringWithFormat:@"My name is %@", name];
+    NSString *japaneseIntroduction = [name stringByAppendingString:@" to moushimasu"];
+    
+    NSLog(@"%@ from f(x)", introduction);
+    NSLog(@"%@ from f(x)", japaneseIntroduction);
+}
+
+- (void)countDownTillIntroduction:(NSInteger)numberOfDays
+{
+    if (numberOfDays == 0)
+    {
+        NSLog(@"The time has come");
+    }
+    else
+    {
+        NSLog(@"%ld days left until introductions", numberOfDays);
+        NSInteger oneLessDay = numberOfDays - 1;
+        [self countDownTillIntroduction: oneLessDay];
+    }
+}
 
 
 
